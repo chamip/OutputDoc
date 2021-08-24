9f2f84fb8059d9bead705d30549bd1bc58608c0b
chamip <824632601@qq.com>
2021-08-24 18:18:59 +0800

    Function overloading of left

--- vimCode/left.cpp
+++ vimCode/left.cpp
@@ -10,12 +10,16 @@
 #include <cstring>
 
 char* left(const char* str, int n = 1);
+unsigned long left(unsigned long num, unsigned count);
+// unsigned long left(unsigned long, unsigned);
 
 const int MaxSize = 100;
 
 int main()
 {
     using namespace std;
+
+    //test of: char* left(const char* str, int n = 1);
     char sample[MaxSize];
     cin.get(sample, MaxSize);
     char* cur = left(sample, 3);
@@ -23,7 +27,15 @@ int main()
     delete[] cur;
     cur = left(sample);
     cout << "cur is: " << cur << endl;
-    delete[] cur;     
+    delete[] cur;
+    cout << endl;
+
+    //test of: unsigned long left(unsigned long, unsigned);  
+    unsigned long num = 12345678;
+    unsigned count = 3;
+    unsigned long ans = left(num, count);
+    cout << "ans: " << ans << endl;
+          
     return 0;
 }
 
@@ -39,4 +51,22 @@ char* left(const char* str, int n) {
     }
     ptr[n] = '\0';
     return ptr;
+}
+
+unsigned long left(unsigned long num, unsigned count) {
+    if(count == 0 || num == 0) {
+        return 0;
+    }
+    int digit = 1;
+    unsigned long n = num;
+    while(n /= 10) {
+        digit++;
+    }
+    if(count < digit) {
+        count = digit - count;
+        while(count--) {
+            num /= 10;
+        }
+    }
+    return num;
 }
