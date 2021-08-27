d118cee8613302a1f166d808d9d91b4ba15520fd
shan.zheng <shan.zheng@thundersoft.com>
2021-08-27 15:58:56 +0800

    hello_std

--- vimCode/hello.cpp
+++ vimCode/hello.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+
 int main()
 {
 	std::cout << "Julia,2021!" << std::endl;
+	bool used[201] = {false};
+	for(int i = 0; i < 201; i++) {
+		std::cout << used[i] << " ";
+		if((i + 1)  % 10 == 0) {
+			std::cout << std::endl;
+		}
+	}
 	return 0;
 }
