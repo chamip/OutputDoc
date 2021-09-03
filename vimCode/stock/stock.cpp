dad5061b4d12e92818d025b3efd136e172fbb369
shan.zheng <shan.zheng@thundersoft.com>
2021-09-03 17:49:51 +0800

    when a object is created first,destructor is called last.

--- vimCode/stock/stock.cpp
+++ vimCode/stock/stock.cpp
@@ -5,8 +5,15 @@
 int main()
 {
     {
-    using std::cout;
-    using std::endl;
+    // using std::cout;
+    // using std::endl;
+
+    /*
+    * 先创建的对象后调用析构函数，和对象调用函数的顺序无关。
+    * note: g++编译之前一定要先保存，不然无法编译。
+    */
+
+    // Stock stock2 = Stock("chamip", 1, 10.0);
     Stock stock1("Julia", 100, 100.0);
     stock1.show();
     stock1.buy(10, 20.0);
