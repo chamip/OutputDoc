bd39bbf7733342662b604e36e8e6fbe6768a7229
shan.zheng <shan.zheng@thundersoft.com>
2021-09-03 18:01:48 +0800

    letter space = 1.

--- vimCode/stock/stock.cpp
+++ vimCode/stock/stock.cpp
@@ -5,30 +5,30 @@
 int main()
 {
     {
-    // using std::cout;
-    // using std::endl;
+        // using std::cout;
+        // using std::endl;
 
-    /*
+        /*
     * 先创建的对象后调用析构函数，和对象调用函数的顺序无关。
     * note: g++编译之前一定要先保存，不然无法编译。
     */
 
-    // Stock stock2 = Stock("chamip", 1, 10.0);
-    Stock stock1("Julia", 100, 100.0);
-    stock1.show();
-    stock1.buy(10, 20.0);
-    stock1.show();
-    stock1.sell(10, 20.0);
-    stock1.show();
-    stock1.update(10.0);
-    stock1.show();
+        // Stock stock2 = Stock("chamip", 1, 10.0);
+        Stock stock1("Julia", 100, 100.0);
+        stock1.show();
+        stock1.buy(10, 20.0);
+        stock1.show();
+        stock1.sell(10, 20.0);
+        stock1.show();
+        stock1.update(10.0);
+        stock1.show();
 
-    Stock stock2 = Stock("chamip", 1, 10.0);
-    stock2.show();
-    stock1 = Stock("syr", 0606, 100.0);
-    stock1.show();
-    stock2.show();
-    std::cout << "Done!" << std::endl;
+        Stock stock2 = Stock("chamip", 1, 10.0);
+        stock2.show();
+        stock1 = Stock("syr", 0606, 100.0);
+        stock1.show();
+        stock2.show();
+        std::cout << "Done!" << std::endl;
     }
     return 0;
 }
