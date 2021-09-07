0b9a735b5f4a19df7ae1f828ad4c3ab1de259fdd
shan.zheng <shan.zheng@thundersoft.com>
2021-09-07 16:17:39 +0800

    operator

---
+++ vimCode/operatorOverload/mytime.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Author: chamip
+ * @Date: 2021-09-07 16:14:30
+ * @LastEditTime: 2021-09-07 16:17:15
+ * @LastEditors: chamip
+ * @Description: function implementation
+ * @FilePath: /OutputDoc/vimCode/operatorOverload/mytime.cpp
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
---
+++ vimCode/operatorOverload/mytime.h
@@ -0,0 +1,25 @@
+/*
+ * @Author: chamip
+ * @Date: 2021-09-07 16:11:19
+ * @LastEditTime: 2021-09-07 16:14:05
+ * @LastEditors: chamip
+ * @Description: define time struct
+ * @FilePath: /OutputDoc/vimCode/operatorOverload/mytime.h
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+
+#ifndef MYTIME_H_
+#define MYTIME_H_
+
+class Time {
+private:
+    int hours;
+    int minutes;
+public:
+    Time();
+    Time(int h, int m = 0);
+    Time operator+(const Time& t) const;
+    ~Time();
+};
+
+#endif
